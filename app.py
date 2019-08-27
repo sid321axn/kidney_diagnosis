b4f59edda578ab953dbca53f180bfa18ce2dac77
sid321axn <sid321axn@gmail.com>
2019-08-27 10:31:37 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,91 @@
+import flask
+import pickle
+import pandas as pd
+from flask import Flask, render_template, request, redirect, url_for
+
+
+# Use pickle to load in the pre-trained model
+with open(f'model/ckd_random_red.pkl', 'rb') as f:
+    model = pickle.load(f)
+
+# Initialise the Flask app
+app = flask.Flask(__name__, template_folder='templates')
+
+@app.route('/')
+def index():
+   return render_template('login.html')
+
+# Route for handling the login page logic
+@app.route('/logins', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'GET':
+        # Just render the initial form, to get input
+       return (render_template('login.html'))
+    
+    if request.method == 'POST':
+        if request.form['username'] == 'admin' or request.form['pass'] == 'P@55w0rd':
+            return redirect(url_for('main'))
+        else:
+            error = 'Invalid Credentials. Please try again.' 
+    return render_template('login.html', error=error)
+
+# Set up the main route
+@app.route('/main', methods=['GET', 'POST'])
+def main():
+    if request.method == 'GET':
+        # Just render the initial form, to get input
+        return(render_template('main.html'))
+    
+    if request.method == 'POST':
+        # Extract the input
+        
+        specific_gravity = request.form.get('specific_gravity')
+        Albumin = request.form.get('Albumin')
+        Blood_Gluc_rand = request.form['Blood_Gluc_rand']
+        Blood_Urea = request.form['Blood_Urea']
+        Serum_Cr = request.form['Serum_Cr']
+        sodium = request.form['sodium']
+        Hemoglobin = request.form['Hemoglobin']
+        packed_cell_volume = request.form['packed_cell_volume']
+        rbc_cnt = request.form['rbc_cnt']
+        htn = request.form.get('htn')
+        diabetes = request.form.get('diabetes')
+       
+        
+        # Make DataFrame for model
+        input_variables = pd.DataFrame([[specific_gravity,Albumin,Blood_Gluc_rand,Blood_Urea,Serum_Cr,sodium,Hemoglobin,packed_cell_volume,rbc_cnt,htn,diabetes]],
+                                       columns=['specific_gravity','Albumin','Blood_Gluc_rand','Blood_Urea','Serum_Cr','sodium','Hemoglobin', 'packed_cell_volume','rbc_cnt','htn', 'diabetes'],
+                                       dtype=float,
+                                       index=['input'])
+
+        # Get the model's prediction
+        prediction = model.predict(input_variables)[0]
+
+        if int(prediction)==1:
+            prediction='Patient may have chronic Kidney Disease'
+        else:
+            prediction='Patient is Healthy !!!!!'
+       
+        # Render the form again, but add in the prediction and remind user
+        # of the values they input before
+        return render_template('main.html',
+                                      original_input={
+                                                     'specific_gravity':specific_gravity,
+                                                     'Albumin':Albumin,
+                                                     'Blood_Gluc_rand':Blood_Gluc_rand,
+                                                     'Blood_Urea':Blood_Urea,
+                                                     'Serum_Cr':Serum_Cr,
+                                                     'sodium':sodium,
+                                                     'Hemoglobin':Hemoglobin,
+                                                     'packed_cell_volume':packed_cell_volume,
+                                                     'rbc_cnt':rbc_cnt,
+                                                     'htn':htn,
+                                                     'diabetes':diabetes,
+                                                     },
+                                     result=prediction
+                                     
+                                     )
+
+if __name__ == '__main__':
+    app.run()
